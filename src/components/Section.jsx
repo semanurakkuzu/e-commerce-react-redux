805f5e354c2e3db7e5d56b394e0a7da76b95bf65
semanurakkuzu <semanurakkuzu@gmail.com>
2022-11-28 15:07:32 +0300

    added section component

---
+++ src/components/Section.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+export default function Section() {
+  return (
+    <div className="row mt-5">
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      
+      
+    </div>
+  );
+}

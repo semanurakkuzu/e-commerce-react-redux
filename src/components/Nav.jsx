366b84b10dd1f80ae7fedfd3d55f7242af1a28d7
semanurakkuzu <semanurakkuzu@gmail.com>
2022-11-28 16:52:42 +0300

    added router

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,9 @@ export default function Nav() {
   return (
     <nav className="navbar bg-light">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1 font-monospace text-warning">
+        <a href="/" className="navbar-brand mb-0 h1 font-monospace text-warning">
           Supreme
-        </span>
+        </a>
       </div>
     </nav>
   );

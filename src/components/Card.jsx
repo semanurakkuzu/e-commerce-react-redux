9f93bb4ecc51a73d0aa939fe8c9414015e2b789a
semanurakkuzu <semanurakkuzu@gmail.com>
2022-11-28 15:07:16 +0300

    added card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+export default function Card(props) {
+  return (
+    <div className="col">
+      <div className="card">
+        <img src={props.img} className="card-img-top" alt={props.name} />
+        <div className="card-body text-center">
+          <h5 className="card-title">{props.name}</h5>
+          <a href="" className="btn btn-warning">
+            Show
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}

c6d298ebdb04de44bdd6b6d1f5d257cde4ee3102
semanurakkuzu <semanurakkuzu@gmail.com>
2022-11-28 15:07:03 +0300

    added nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1 font-monospace text-warning">
+          Supreme
+        </span>
+      </div>
+    </nav>
+  );
+}

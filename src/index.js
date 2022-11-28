7570201248857515de6a8cfbf313a0f4aa59e83d
semanurakkuzu <semanurakkuzu@gmail.com>
2022-11-28 15:59:16 +0300

    added router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 import './index.css';
 import App from './App';
 
@@ -8,9 +12,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/clothesdetails",
+      element: <ClothesDetails/>,
+    },
+  ]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
+root.render(<RouterProvider router={router} />
 );
 
 

79b81e145193f613554f3a3b81cad12f4e0f3eb2
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-22 16:09:30 +0300

    added  addtocart button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,12 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { addToCart} from '../redux/cartSlice'
 import { Link } from "react-router-dom";
 
+
 export default function Card(props) {
+  const dispatch = useDispatch()
+
   return (
     <div className="card">
       <img src={props.img} className="card-img-top" alt={props.name} />
@@ -9,9 +15,9 @@ export default function Card(props) {
         <Link className="btn btn-warning" to={`/clothes/${props.id}`}>
           Show more
         </Link>
-        <Link className="btn btn-warning ms-3" to={`/clothes/${props.id}`}>
+        <button className="btn btn-warning ms-3" onClick={() => dispatch(addToCart())}>
         Add to cart
-        </Link>
+        </button>
       </div>
     </div>
   );

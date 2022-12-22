c7f1f0f0d90822604128109384e33444a314a6ba
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-22 16:52:24 +0300

    added name and img

--- src/redux/cart.js
+++ src/redux/cart.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { deleteToCart} from '../redux/cartSlice'
-export default function Cart({id,name,img}) {
-    const cart = useSelector((state) => state.cart.id)
+export default function Cart() {
     const dispatch = useDispatch()
-  
+    const name = useSelector((state) => state.data.name)
+    const img = useSelector((state) => state.data.variants.image)
 
   return (
     <div>
       <div className='container'>
-            <div class="card mb-3" style={{maxWidth: "540px"}} itemID={id}>
+            <div class="card mb-3">
             <div class="row g-0">
                 <div class="col-md-4">
                 <img src={img} class="img-fluid rounded-start" alt={name}/>

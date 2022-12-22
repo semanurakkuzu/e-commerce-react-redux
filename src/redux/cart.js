c900d8bdc92645c3a398315b387982fbae30e6b5
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-22 16:08:41 +0300

    added state

--- src/redux/cart.js
+++ src/redux/cart.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './cartSlice'
-
-export default function Cart() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
+import { deleteToCart} from '../redux/cartSlice'
+export default function Cart({id,name,img}) {
+    const cart = useSelector((state) => state.cart.id)
+    const dispatch = useDispatch()
+  
 
   return (
     <div>
       <div className='container'>
-            <div class="card mb-3" style="max-width: 540px;">
+            <div class="card mb-3" style={{maxWidth: "540px"}} itemID={id}>
             <div class="row g-0">
                 <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="..."/>
+                <img src={img} class="img-fluid rounded-start" alt={name}/>
                 </div>
                 <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <button type="button" class="btn btn-danger">Delete
+                    <h5 class="card-title">{name}</h5>
+                    <button type="button" class="btn btn-danger" onClick={() => dispatch(deleteToCart())}>Delete
                     </button>
                 </div>
                 </div>

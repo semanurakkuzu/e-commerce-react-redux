631224ca76e1655e0be033baae12f22ce99b32ec
semanurakkuzu <semanurakkuzu@gmail.com>
2022-12-22 18:21:54 +0300

    Add to link

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { addToCart} from '../redux/cartSlice'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
 import { Link } from "react-router-dom";
 
-
 export default function Card(props) {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   return (
     <div className="card">
@@ -15,10 +14,14 @@ export default function Card(props) {
         <Link className="btn btn-warning" to={`/clothes/${props.id}`}>
           Show more
         </Link>
-        <button className="btn btn-warning ms-3" onClick={() => dispatch(addToCart())}>
-        Add to cart
-        </button>
+        <Link
+          className="btn btn-warning ms-3"
+          onClick={() => dispatch(addToCart(props))}
+        >
+          Add to cart
+        </Link>
       </div>
     </div>
+    
   );
 }
